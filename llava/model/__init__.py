8d6fb00c81a79641eaaaf35098efdc502d0e9acc
J1shen <jyshen44@gmail.com>
2023-12-29 18:49:40 +0800

    delect llava

--- llava/model/__init__.py
+++
@@ -1,3 +0,0 @@
-#from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
-from .language_model.cached_llama import LlavaLlamaForCausalLM, LlavaConfig
-#from .language_model.llava_mpt import LlavaMPTForCausalLM, LlavaMPTConfig
